3638d10643259ce8a4547aee44049bd27a92a686
ljm250 <jiamianliu@outlook.com>
2019-04-16 18:47:08 +0200

    add SMS

---
+++ Student/tempCodeRunnerFile.py
@@ -0,0 +1,12 @@
+# Modelling Algorithms
+# from sklearn.tree import DecisionTreeClassifier
+# from sklearn.linear_model import LogisticRegression
+# from sklearn.neighbors import KNeighborsClassifier
+# from sklearn.naive_bayes import GaussianNB
+# from sklearn.svm import SVC, LinearSVC
+# from sklearn.ensemble import RandomForestClassifier , GradientBoostingClassifier
+
+# # Modelling Helpers
+# from sklearn.preprocessing import Imputer , Normalizer , scale
+# from sklearn.cross_validation import train_test_split , StratifiedKFold
+# from sklearn.feature_selection import RFECV
